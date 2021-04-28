7d8a3b3996ebf499a5785741ac1a8ba6af7a578a
Your Name <victor.meynaud@chartes.psl.eu>
2021-04-28 11:33:33 +0200

    commencé test

--- stat_eml.py
+++ stat_eml.py
@@ -105,7 +105,7 @@ def trouver_url(texte):
                     #pass
             #else:
                 #pass
-        return liste_uri, liste_statut, liste_date_test, liste_pers      
+        return liste_uri, liste_statut, liste_date_test, liste_pers   
 
 def liste_en_str(liste):
     if len(liste) == 1:
@@ -150,18 +150,23 @@ def url_wayback(url):
         de la requête. 
     """
     url_api = "http://archive.org/wayback/available?url=" + url
-    req = requests.get(url_api, timeout=5)
-    resp = json.loads(str(req.text))
-    print(resp)
-    if resp["archived_snapshots"]:
-        status = resp["archived_snapshots"]["closest"]["status"]
-        print(status)
-        available = resp["archived_snapshots"]["closest"]["available"]
-        url_2 = resp["archived_snapshots"]["closest"]["url"]
-        time = resp["archived_snapshots"]["closest"]["timestamp"]
-        return status, available, url_2, time
-    else:
-        return None
+    try:
+        req = requests.get(url_api, timeout=5)
+        resp = json.loads(str(req.text))
+    except requests.exceptions.ReadTimeout: # Si l'API a mis trop de temps à répondre
+        pass
+    # Si elle ne trouve rien, l'API retourne un dict vide. 
+    try:
+        if resp["archived_snapshots"] != {}:
+            status = resp["archived_snapshots"]["closest"]["status"]
+            available = resp["archived_snapshots"]["closest"]["available"]
+            url_2 = resp["archived_snapshots"]["closest"]["url"]
+            time = resp["archived_snapshots"]["closest"]["timestamp"]
+            return str(status), str(available), str(url_2), str(time)
+        else: # Si l'API retourne une liste vide
+            return None, None, None, None
+    except UnboundLocalError: # Si l'API a mis trop de temps à répondre
+        return None, None, None, None
 
 def traitement_nlt(texte): 
     """ xxx
@@ -222,79 +227,96 @@ def extraire_contenu_mail(mail):
         ep = eml_parser.EmlParser(include_raw_body=True, include_attachment_data=False)
         parsed_eml = ep.decode_email_bytes(raw_email)
         return parsed_eml
-  
-with open(os.path.join(chemin_actuel,"perso","test_wayback.csv"), 'w') as f:
-    writer = csv.writer(f, delimiter = ";")
-    liste_col = ['nom_fichier', 'top_trois_mots', 'url(s)', 'resultat_test_URL', 'date_test_URL', 'responsable_URL', "internet_archive_status"
-                 , "internet_archive_dispo", "internet_archive_url", "internet_archive_timestamp"]
-    writer.writerow(liste_col)   
-    mail = 0
-    nb_url = 0
-    nb_noms = 0    
-    for root, dirs, files in os.walk(os.path.join(chemin_actuel,"perso","mail_enc"), topdown=True):
-        for index, name in enumerate(files):
-            filename = os.path.join(root, name)
-            if filename.endswith(".eml"):
-                mail += 1
-                liste_val = []
-                data = extraire_contenu_mail(filename)
-                texte = data["body"][0]["content"]
-                compte_nom, liste_noms = trouver_noms_propres(texte)
-                if liste_noms:
-                    #print(liste_noms)
-                    for nom in liste_noms:
-                        texte = texte.replace(nom, "")
-                nb_noms += compte_nom
-                liste_val.append(filename)
-                top = traitement_nlt(texte)
-                #print(top)
-                string = ','.join(str(valeur) for valeur in top)
-                liste_val.append(string)
-                try:
-                    liste_test = []
-                    # On ne lance pas la fonction sur texte_sans_nom au cas où des noms auraient été supprimés d'URL
-                    liste_uri, liste_statut, liste_date_test, liste_pers = trouver_url(texte)
-                    # print(trouver_url(texte))
-                    #if liste_uri is not None and liste_statut is not None and liste_date_test is not None and liste_pers is not None:
-                    nb_url += 1
-                    # liste_uri sera une liste vide si il n'y avait dans le mail que des URL marquées comme à éviter
-                    if len(liste_uri) != 0:
-                        uri = liste_en_str(liste_uri)
-                        statut = liste_en_str(liste_statut)
-                        date_test = liste_en_str(liste_date_test)
-                        pers = liste_en_str(liste_pers)
-                        liste_test.append(uri)
-                        liste_test.append(str(statut))
-                        liste_test.append(date_test)
-                        liste_test.append(pers)
-                        liste_wb = []
-                        liste_stat_wb = []
-                        liste_avai_wb = []
-                        liste_lien_wb = []
-                        liste_time_wb = []
-                        for uris in liste_uri:
-                            status, available, lien, time = url_wayback(uris)
-                            liste_stat_wb.append(status)
-                            liste_avai_wb.append(available)
-                            liste_lien_wb.append(lien)
-                            liste_time_wb.append(time)
-                        stat_wb_str = liste_en_str(liste_stat_wb)
-                        avai_wb_str = liste_en_str(liste_avai_wb)
-                        lien_wb_str = liste_en_str(liste_lien_wb)
-                        time_wb_str = liste_en_str(liste_time_wb)
-                        liste_wb.append(stat_wb_str)
-                        liste_wb.append(avai_wb_str)
-                        liste_wb.append(lien_wb_str)
-                        liste_wb.append(time_wb_str)
-                # Si aucune URL a été trouvée dans le mail, trouver_url retourne None
-                except TypeError:
-                    pass
-                if liste_test: 
-                    liste_val = liste_val + liste_test + liste_wb
-                #if len(liste_val) > 2:
-                    #print(liste_val[3])
-                writer.writerow(liste_val)
-    print("Nombre de mails traités : " + str(mail))
-    print("Nombre d'URL traitées : " + str(nb_url))
-    print("Nombre d'éléments détectés comme des noms propres : " + str(nb_noms))
-    print("Temps de calcul (en secondes) : " + str(time.time() - start_time))
+
+def traiter_mails(source, output):
+    with open(output, 'w') as f:
+        writer = csv.writer(f, delimiter = ";")
+        liste_col = ['nom_fichier', 'top_trois_mots', 'url(s)', 'resultat_test_URL', 'date_test_URL', 'responsable_URL', "internet_archive_status"
+                    , "internet_archive_dispo", "internet_archive_url", "internet_archive_timestamp"]
+        writer.writerow(liste_col)   
+        mail = 0
+        nb_url = 0
+        nb_noms = 0 
+        nb_wb = 0   
+        for root, dirs, files in os.walk(source, topdown=True):
+            for index, name in enumerate(files):
+                filename = os.path.join(root, name)
+                if filename.endswith(".eml"):
+                    mail += 1
+                    liste_val = []
+                    data = extraire_contenu_mail(filename)
+                    texte = data["body"][0]["content"]
+                    compte_nom, liste_noms = trouver_noms_propres(texte)
+                    if liste_noms:
+                        #print(liste_noms)
+                        for nom in liste_noms:
+                            texte = texte.replace(nom, "")
+                    nb_noms += compte_nom
+                    liste_val.append(filename)
+                    top = traitement_nlt(texte)
+                    #print(top)
+                    string = ','.join(str(valeur) for valeur in top)
+                    liste_val.append(string)
+                    try:
+                        liste_test = []
+                        # On ne lance pas la fonction sur texte_sans_nom au cas où des noms auraient été supprimés d'URL
+                        liste_uri, liste_statut, liste_date_test, liste_pers = trouver_url(texte)
+                        # print(trouver_url(texte))
+                        #if liste_uri is not None and liste_statut is not None and liste_date_test is not None and liste_pers is not None:
+                        nb_url += 1
+                        # liste_uri sera une liste vide si il n'y avait dans le mail que des URL marquées comme à éviter
+                        if len(liste_uri) != 0:
+                            uri = liste_en_str(liste_uri)
+                            statut = liste_en_str(liste_statut)
+                            date_test = liste_en_str(liste_date_test)
+                            pers = liste_en_str(liste_pers)
+                            liste_test.append(uri)
+                            liste_test.append(str(statut))
+                            liste_test.append(date_test)
+                            liste_test.append(pers)
+                            liste_wb = []
+                            liste_stat_wb = []
+                            liste_avai_wb = []
+                            liste_lien_wb = []
+                            liste_time_wb = []
+                            for uris in liste_uri:
+                                status, available, lien, time = url_wayback(uris)
+                                if status is not None:
+                                    liste_stat_wb.append(status)
+                                    liste_avai_wb.append(available)
+                                    liste_lien_wb.append(lien)
+                                    liste_time_wb.append(time)
+                                else:
+                                    pass
+                            try:
+                                nb_wb += len(liste_stat_wb)
+                                stat_wb_str = liste_en_str(liste_stat_wb)
+                                avai_wb_str = liste_en_str(liste_avai_wb)
+                                lien_wb_str = liste_en_str(liste_lien_wb)
+                                time_wb_str = liste_en_str(liste_time_wb)
+                                liste_wb.append(stat_wb_str)
+                                liste_wb.append(avai_wb_str)
+                                liste_wb.append(lien_wb_str)
+                                liste_wb.append(time_wb_str)
+                            except NameError:
+                                pass
+                    # Si aucune URL a été trouvée dans le mail, trouver_url retourne None
+                    except TypeError:
+                        pass
+                    try: 
+                        liste_val = liste_val + liste_test + liste_wb
+                    except:
+                        pass
+                    #if len(liste_val) > 2:
+                        #print(liste_val[3])
+                    writer.writerow(liste_val)
+        print("Nombre de mails traités : " + str(mail))
+        print("Nombre d'URL traitées : " + str(nb_url))
+        print("Nombre d'éléments détectés comme des noms propres : " + str(nb_noms))
+        print("Nombre de requêtes faites à Internet Archive : " + str(nb_wb))
+        print("Temps de calcul (en secondes) : " + str(time.time() - start_time))
+
+
+output = os.path.join(chemin_actuel,"perso","test_wayback_2804a.csv")
+source = os.path.join(chemin_actuel,"perso","mail_enc")
+traiter_mails(source, output)
---
+++ test.py
@@ -0,0 +1,86 @@
+import emails, PyPDF2, random, os
+from os.path import dirname, abspath
+import subprocess
+from odf.opendocument import OpenDocumentText
+from odf.text import P
+from stat_eml import traiter_mails
+
+chemin_actuel = dirname(abspath(__file__))
+rand = random.randint(0, 10)
+nb_mails = random.randint(100, 200)
+rand_2 = random.randint(0, 10)
+url_OK = "https://guthib.com/"
+url_NOK = "https://guthib.com/nok"
+adress = ["timlinux@hotmail.com",
+"timtroyr@me.com",
+"smcnabb@live.com",
+"gozer@outlook.com",
+"kohlis@att.net",
+"dgatwood@icloud.com",
+"snunez@me.com",
+"lydia@aol.com",
+"imightb@outlook.com",
+"danny@aol.com",
+"dieman@aol.com",
+"gator@me.com"]
+
+
+def create_dummy_pdf(nombre):
+    for x in range(1,nombre):
+        bashCommand = "dummypdf -f ./dummy/test_{a}.pdf -n {b}".format(a=str(x), b=str(random.randint(1, 20)))
+        process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+        output, error = process.communicate()
+
+lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nibh eros, finibus nec nisl id, rhoncus fringilla ligula. Aenean vel eleifend urna. Quisque a est vestibulum, semper elit ac, fermentum nisl. Donec mattis purus id turpis porttitor, non fringilla dolor dictum. Morbi aliquet, felis vulputate hendrerit fermentum, odio nulla dignissim ex, non euismod elit urna eu augue. Etiam vel quam dui. Sed dictum fermentum eros, et bibendum tortor cursus nec. Maecenas et nunc mollis urna cursus cursus. Donec a ullamcorper ipsum."
+      
+def create_dummy_odt(lorem, iter, rand):
+    for x in range(1, iter):
+        textdoc = OpenDocumentText()
+        texte = lorem * rand
+        p = P(text=texte)
+        textdoc.text.addElement(p)
+        textdoc.save("./dummy/test_{a}".format(a=str(x)), True)
+
+def create_dummy_mails(lorem, iter, rand):
+    liste_odt = []
+    liste_pdf = []
+    for root, dirs, files in os.walk(os.path.join("./dummy")):
+        for name in files:
+            filename = os.path.join(root, name)
+            if filename.endswith(".odt"):
+                liste_odt.append(str(filename))
+            elif filename.endswith(".pdf"):
+                liste_pdf.append(str(filename))
+            else:
+                pass
+    for x in range(1, iter):
+        nb = random.randint(0, 1)
+        nb_lien = random.randint(0, 5)
+        pos = random.randint(0, len(lorem)-1)
+        lorem = (lorem[:pos] + ' ' + url_OK + " " + lorem[pos:] + lorem[:pos] + ' ' + url_NOK + " " + lorem[pos:])*nb_lien
+        if type(x//2) is not float:
+            message = emails.html(html="<p>{a}</p>".format(a=(lorem*rand)),
+                                subject=lorem[:20] + "_" + str(iter),
+                                mail_from=('Some Name', adress[random.randint(0, len(adress)-1)]))
+            if nb == 0:
+                random_odt = random.choice(liste_odt)
+                message.attach(data=open(random_odt, 'rb'), filename=random_odt)
+            else:
+                random_pdf = random.choice(liste_pdf)
+                message.attach(data=open(random_pdf, 'rb'), filename=random_pdf)
+        else:
+            message = emails.html(html="<p>{a}</p>".format(a=(lorem*rand)),
+                                subject=lorem[:20] + "_" + str(iter),
+                                mail_from=('Some Name', adress[random.randint(0, len(adress)-1)]))
+        m = message.as_message()
+        s = m.as_string()
+        with open("./dummy/dummy_mails/test_{a}.eml".format(a=str(x)), "w") as mail:
+            mail.write(s)
+        
+
+create_dummy_pdf(rand)
+create_dummy_odt(lorem, rand, rand_2)
+create_dummy_mails(lorem, nb_mails, rand_2)
+output = os.path.join(chemin_actuel,"resultat","resultat_test.csv")
+source = os.path.join(chemin_actuel,"dummy","dummy_mails")
+traiter_mails(source, output)
